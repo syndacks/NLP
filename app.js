9977006b090294bbb8476e8e79f39ea8baeba6da
Syndacks <math.milliken@gmail.com>
2016-09-07 16:54:49 -0700

    theme URL scrape

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ var cheerio = require("cheerio");
 var natural = require("natural");
 
 //scrape Wikipedia Article (Theme) and retrieve all links
-request()
+themeURL = "https://en.wikipedia.org/wiki/Jazz";
+
+request(themeURL, function(error, response, body){
+	if(!error){
+		var $ = cheerio.load(body);
+
+      $('#mw-content-text a').each(function() {
+        var text = $(this).text();
+        var link = "http:/en.wikipedia.org"+$(this).attr('href');
+		console.log(text + ": " + link);
+		})
+	}
+});
+
+
 
 
 //scrape Wikipedia Article by passing in URL
@@ -36,9 +50,9 @@ function aylien(criteria){
 
 var criteria_terms = ['genre', 'musician', 'instrument', 'deadmau5', 'jazz', 'Armstrong']
 
-for(i=0; i<criteria_terms.length; i++){
-	aylien(criteria_terms[i]);
-};
+// for(i=0; i<criteria_terms.length; i++){
+// 	aylien(criteria_terms[i]);
+// };
 
 
 
@@ -47,10 +61,3 @@ for(i=0; i<criteria_terms.length; i++){
 
 
 
-//process the articleBody with Natural module
-// var TfIdf = natural.TfIdf;
-// var tfidf = new TfIdf();
-
-// tfidf.listTerms(0 document index).forEach(function(item) {
-//     console.log(item.term + ': ' + item.tfidf);
-// });
